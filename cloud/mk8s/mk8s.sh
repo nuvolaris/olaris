d7eb4211873726bd9372b10b8ca9e1d30390659c
Michele Sciabarra <michele@nuvolaris.io>
2023-09-18 16:59:47 +0100

    testing fixes

--- cloud/mk8s/mk8s.sh
+++ cloud/mk8s/mk8s.sh
@@ -15,22 +15,52 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-IP="$1"
+SERVER="$1"
 # if not provided try to retrieve the public it
-if test -z "$IP"
-then IP="$(curl https://ipecho.net/plain)"
+if test -z "$SERVER"
+then SERVER="$(curl https://ipecho.net/plain)"
 fi
-if test -z "$IP"
-then IP="$(curl ifconfig.me)"
+if test -z "$SERVER"
+then SERVER="$(curl ifconfig.me)"
 fi
+export SERVER
 # install microk8s
 apt-get update
 apt-get install -y snapd curl grep sudo
 snap install microk8s --classic
 microk8s stop
-sed -i "/#MOREIPS/a IP.10 = $IP" /var/snap/microk8s/current/certs/csr.conf.template
+cp /var/snap/microk8s/current/certs/csr.conf.template /tmp/in
+# script to try to give accettable values for ip and dns name
+
+python3 <<EOF >/var/snap/microk8s/current/certs/csr.conf.template
+import os, socket, re
+ip_address = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+server = os.environ.get("SERVER")
+if re.match(ip_address, server):
+        host_ip = server
+        host_name = f"{server}.nip.io"
+else:   
+        host_name = server
+        host_ip = socket.gethostbyname(server)
+with open("/tmp/in", 'r') as f: lines = f.readlines()
+ip = -1
+dns= -1
+for i in range(len(lines)-1, 0, -1): 
+    if ip == -1 and lines[i].startswith("IP."): ip = i
+    if dns == -1 and lines[i].startswith("DNS."): dns = i
+for i in range(0, len(lines)):
+    print(lines[i],end="")
+    if i == ip:
+        n = int(lines[ip].split(".")[1].split(" ")[0])+1
+        print(f"IP.{n} =", host_ip)
+    if i == dns:
+        n = int(lines[dns].split(".")[1].split(" ")[0])+1
+        print(f"DNS.{n} =", host_name )
+EOF
+
 microk8s start
 while microk8s kubectl get nodes | grep NotReady
 do echo Waiting for Ready ; sleep 5
 done
 microk8s enable hostpath-storage dns ingress cert-manager
+
